064093eacb4ae8b890362d4042a04855ceea2f12
Ilya <ilya.badaneen@gmail.com>
2020-04-15 17:59:57 +0000

    tests

---
+++ tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bunt/Desktop/projects/cpphw2/tests/unit/lib/googletest
+# Build directory: /home/bunt/Desktop/projects/cpphw2/tests/unit/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
