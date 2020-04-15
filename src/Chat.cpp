f3198476b983a07ef1eaa63546c19c6ef563dfb9
nathanKith <ntolparov@gmail.com>
2020-04-15 17:44:30 +0300

    finish chat and message

--- src/Chat.cpp
+++ src/Chat.cpp
@@ -1,19 +1,18 @@
 #include "Сhat.h"
 
-Chat::Chat(unsigned int user1Id, unsigned int user2Id, std::string database) {
-    user1Id = user1Id;
-    user2Id = user2Id;
-    database = database;
-}
+Chat::Chat(unsigned int user1Id, unsigned int user2Id, std::string database)
+    : user1Id(user1Id)
+    , user2Id(user2Id)
+    , database(database) {}
 
 int Chat::SendMessage(unsigned int charId, unsigned int sender, std::string text) {
-
+    return 0;
 }
 
 Chat& Chat::GetChat(unsigned int chatId) {
-
+    throw NULL;
 }
 
 std::vector<Chat> Chat::GetAllMyChats(unsigned int userId) {
-
+    throw NULL;
 }
