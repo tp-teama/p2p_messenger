9ca2f8063174a34e63a9a0ada1c21c3fd8a74062
Amaterasy17 <sheb70017@gmail.com>
2020-05-13 16:33:33 +0300

    Fix client

--- src/Client.cpp
+++ src/Client.cpp
@@ -7,7 +7,13 @@ Client::Client(): sock(service){
 void Client::Connect(int port, std::string Ip) {
     tcp::endpoint ep( ip::address::from_string(Ip), port);
     sock.open(ip::tcp::v4());
-    sock.async_connect(ep, [&](){});
+    sock.async_connect(ep, [&](error_code ec){
+        if (ec) {
+            std::cerr << "Connect is failed: " << ec.message() << "\n";
+        } else{
+            std::cout << "Connect is fine" << std::endl;
+        }
+    });
 }
 
 void Client::Close() {
