c52cd5ffe76c223fe7900002c8e97c9e67de8ae3
m.ershov <m.ershov@avtopass.net>
2020-05-18 02:20:46 +0300

    storage: +add:new methods

--- include/Storage.h
+++ include/Storage.h
@@ -3,11 +3,18 @@
 
 using namespace std;
 
+struct User{
+    int user_id;
+    string username;
+};
+
 class Storage{
 public:
     Storage() : database(nullptr){}
     int Connect();
     int InitDB();
+    int AddUser(string username);
+    User GetUser(int id);
     void CloseDB();
 private:
     pqxx::connection *database;
--- src/Storage.cpp
+++ src/Storage.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <pqxx/pqxx>
+#include <string>
 
 using namespace std;
 
@@ -50,6 +51,66 @@ int Storage::InitDB() {
 
     return 0;
 }
+
+
+int Storage::AddUser(string username) {
+    string sql_user;
+
+    try {
+        pqxx::connection conn("dbname = tp_mess user = postgres password = docker hostaddr = 127.0.0.1 port = 5432");
+        if (conn.is_open()) {
+            cout << "Opened database successfully: " << conn.dbname() << endl;
+        } else {
+            cout << "Can't open database" << endl;
+            return 1;
+        }
+        sql_user = "INSERT INTO users (username) "  \
+         "VALUES ('" + username + "'); ";
+
+        pqxx::work W(conn);
+
+        W.exec( sql_user );
+        W.commit();
+        cout << "Inserted successfully" << endl;
+        (*database).disconnect();
+    } catch (const std::exception &e) {
+        cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
+
+User Storage::GetUser(int id) {
+    string sql;
+    User user = User{};
+
+    try {
+        pqxx::connection conn("dbname = tp_mess user = postgres password = docker hostaddr = 127.0.0.1 port = 5432");
+        if (conn.is_open()) {
+            cout << "Opened database successfully: " << conn.dbname() << endl;
+        } else {
+            cout << "Can't open database" << endl;
+            return User{};
+        }
+
+        sql = "SELECT * from users where user_id = " + to_string(id);
+        pqxx::nontransaction N(conn);
+        pqxx::result R( N.exec( sql ));
+
+        for (pqxx::result::const_iterator c = R.begin(); c != R.end(); ++c) {
+            user.user_id = c[0].as<int>();
+            user.username = c[1].as<string>();
+        }
+        cout << "Select success" << endl;
+        conn.disconnect();
+    } catch (const std::exception &e) {
+        cerr << e.what() << std::endl;
+        return User{};
+    }
+    return user;
+}
+
 
 void Storage::CloseDB() {
     (*database).disconnect();
